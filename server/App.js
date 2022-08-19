7528bc97958b183f2e48280842915d358837cc1f
Yuheng Xiao <qq756951570@gmail.com>
2022-08-19 15:07:27 -0400

    server_npm_start

--- server/App.js
+++ server/App.js
@@ -1,29 +1,16 @@
 const express = require('express');
-const app = express();
-const configRoutes = require('./routes');
-//cors = require("cors")
-const cors = require('cors');
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').config()
-// }
-const corsOptions ={
-    //origin:'http://localhost:3000', 
-    origin:'https://klouie1092.github.io', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+const cors = require('cors')
 
-app.use(function(req, res, next) {
-  //res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
-  res.header("Access-Control-Allow-Origin", "https://klouie1092.github.io"); 
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use(express.json());
-app.use(cors())
+const app = express();
+
+app.use(cors());
+const configRoutes = require('./routes');
+// app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 configRoutes(app);
-app.listen(process.env.PORT || 4000, () => {
+
+app.listen(3001, () => {
   console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:4000');
+  console.log('Your routes will be running on http://localhost:3001');
 });
