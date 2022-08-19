7e8fabc23dea791348802af3edb872aece12b0d8
Yuheng Xiao <qq756951570@gmail.com>
2022-08-19 14:58:50 -0400

    server_users

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,7 +4,25 @@ const usersData = require('../data/users');
 let { ObjectId } = require('mongodb');
 const xss = require('xss');
 
+router.post('/createUser', async (req,res) =>{
 
+});
+
+router.post('/checkUserPassword', async (req,res) =>{
+
+});
+
+router.post('/checkAdminFlag', async (req,res) =>{
+
+});
+
+router.get('/getUserById', async (req,res) =>{
+
+});
+
+router.post('/deleteUser', async (req,res) =>{
+
+});
 
 
 module.exports = router;
