fb11fac8f1cf01132f0245010e52ee281e2eff37
Alexander Kulpin <alexandtoddy@gmail.com>
2022-08-20 11:14:03 -0400

    Backend Sending Data
    Now able to press submit to send data to MongoDB, currently only sends whatever the text that was read, later adding the saving of an image hash

--- server/routes/images.js
+++ server/routes/images.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const imagesData = require('../data/images');
+const data = require('../data');
+const imagesData = data.images;
 let { ObjectId } = require('mongodb');
 const xss = require('xss');
+
+router.post('/setImage', async (req,res) =>{
+	try 
+	{
+		console.log("out", req.body);
+		const image = await imagesData.setImage(req.body.text);
+		//console.log(image);
+		//res.json(image);
+	} 
+	catch (e) 
+	{
+		res.status(404).json({error: e});
+	}
+});
 
 router.get('/getAllUnapprovedImages', async (req,res) =>{
 
