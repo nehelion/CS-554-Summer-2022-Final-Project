2f4fdf4edd85bc07f164b2f50928f9895976a6e1
Yuheng Xiao <qq756951570@gmail.com>
2022-08-20 15:21:15 -0400

    approvedImage

--- client/src/components/ApprovedImage.js
+++ client/src/components/ApprovedImage.js
@@ -49,7 +49,7 @@ const ApprovedImage = () =>{
         //   }
         // }
         setLoading(false);
-        if (imageInfo == false){
+        if (!imageInfo ||  imageInfo === false){
             throw "Image has been deleted or can't find!";
         }
       } catch (e) {
