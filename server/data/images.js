a849b8c883fafaf206184dc293a14a30e4b07cfa
Yuheng Xiao <qq756951570@gmail.com>
2022-08-22 15:32:53 -0400

    image_server

--- server/data/images.js
+++ server/data/images.js
@@ -38,21 +38,46 @@ async function getAllApprovedImages() {
   return approvedImagesList;
 }
 
-async function getImageByImageId() {
-
+async function getImageByImageId(id) {
+  if (!id) throw "No userId";
+  const imagesCollection = await images();
+  const image = await imagesCollection.findOne({_id: ObjectId(id)});
+  if (image === null) throw "No image with that id";
+  return image;
 }
 
 async function getImageByUserId() {
 
 }
 
-async function editImageByImageId() {
-
+async function editImageByImageId(id, text) {
+  if (!id || !text) throw "No edit input!";
+  const oldImage = await getImageByImageId(id);
+  let newImage = {
+    _id: ObjectId(id),
+    url: oldImage.url,
+    tag: oldImage.tag,
+    text: text,
+    approval: oldImage.approval
+  };
+  const imagesCollection = await images();
+  const updatedInfo = await imagesCollection.updateOne(
+    { _id: ObjectId(id) },
+    { $set: newImage }
+  );
+  if (!updatedInfo.matchedCount && !updatedInfo.modifiedCount) {
+    throw "could not update image successfully";
+  }
+  return true;
 }
 
 
-async function deleteImageByImageId() {
-
+async function deleteImageByImageId(id) {
+  if (!id) throw "No delete id!";
+  const imagesCollection = await images();
+  const deletionInfo = await imagesCollection.deleteOne({_id: ObjectId(id)});
+  if (deletionInfo.deletedCount === 0) throw `Could not delete this image`;
+  return true;
 }
 
 module.exports = {
--- server/routes/images.js
+++ server/routes/images.js
@@ -39,19 +39,46 @@ router.get('/getAllApprovedImages', async (req,res) =>{
 });
 
 router.get('/getImageByImageId/:id', async (req,res) =>{
-
+	try {
+		const id = req.params.id;
+		if (!id) throw 'Empty userId!';
+		let x = await imagesData.getImageByImageId(id);
+		res.status(200).json(x);
+	} 
+	catch (e) {
+		throw e;
+	}
 });
 
 router.get('/getImageByUserId/:id', async (req,res) =>{
 
 });
 
-router.post('/editImageByImageId/:id', async (req,res) =>{
-
+router.post('/editImageByImageId', async (req,res) =>{
+	try {
+		var body = req.body;
+		if (!body._id || !body.edit) throw 'Empty new text input!';
+		const id = xss(body._id);
+		const text = xss(body.edit);
+		let x = await imagesData.editImageByImageId(id, text);
+		res.status(200).json(x);
+	} 
+	catch (e) {
+		res.status(500).json({"1":e});
+	}
 });
 
-router.post('/deleteImageByImageId/:id', async (req,res) =>{
-
+router.post('/deleteImageByImageId', async (req,res) =>{
+	try {
+		var body = req.body;
+		if (!body._id) throw 'Empty delete input!';
+		const id = xss(body._id);
+		let x = await imagesData.deleteImageByImageId(id);
+		res.status(200).json(x);
+	} 
+	catch (e) {
+		res.status(500).json({"1":e});
+	}
 });
 
 
